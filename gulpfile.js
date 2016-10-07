0d7bf0b6ec6d2d906ba84675f500a735b2a10396
Luan Costa <luanfelipecosta@gmail.com>
2016-10-07 20:56:13 -0300

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -14,11 +14,13 @@ var messages = {
 /*
  * Build the Jekyll Site
  */
+
+ 
 gulp.task('jekyll-build', function (done) {
     browserSync.notify(messages.jekyllBuild);
     return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
         .on('close', done);
-        
+
 });
 /**
  * Rebuild Jekyll & do page reload
